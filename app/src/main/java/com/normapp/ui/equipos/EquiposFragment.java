8356e592921d8e0e3a3240b38561793bcf7f0e48
Felipe Villamizar <felipevipa@outlook.com>
2022-08-03 17:09:15 -0500

    Added Equipos files

--- app/src/main/java/com/normapp/ui/equipos/EquiposFragment.java
+++ app/src/main/java/com/normapp/ui/equipos/EquiposFragment.java
@@ -1,4 +1,4 @@
-package com.normapp.ui.dashboard;
+package com.normapp.ui.equipos;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,22 +10,22 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.normapp.databinding.FragmentDashboardBinding;
+import com.normapp.databinding.FragmentEquiposBinding;
 
-public class DashboardFragment extends Fragment {
+public class EquiposFragment extends Fragment {
 
-    private FragmentDashboardBinding binding;
+    private FragmentEquiposBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        DashboardViewModel dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
+        EquiposViewModel equiposViewModel =
+                new ViewModelProvider(this).get(EquiposViewModel.class);
 
-        binding = FragmentDashboardBinding.inflate(inflater, container, false);
+        binding = FragmentEquiposBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        final TextView textView = binding.textEquipos;
+        equiposViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
--- app/src/main/java/com/normapp/ui/equipos/EquiposViewModel.java
+++ app/src/main/java/com/normapp/ui/equipos/EquiposViewModel.java
@@ -1,16 +1,16 @@
-package com.normapp.ui.dashboard;
+package com.normapp.ui.equipos;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class DashboardViewModel extends ViewModel {
+public class EquiposViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public DashboardViewModel() {
+    public EquiposViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is dashboard fragment");
+        mText.setValue("This is equipos fragment");
     }
 
     public LiveData<String> getText() {
